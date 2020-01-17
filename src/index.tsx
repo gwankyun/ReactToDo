a9ebcdb9044e122ec64671e2ec77cd8bf39dc0f0
權少 <ljqican@163.com>
2020-01-17 23:28:34 +0800

    數組改用Immutale.List

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FC } from 'react';
-import 'immutable'
+import Immutable from 'immutable'
 import ReactDOM from 'react-dom';
 import './index.css';
 // import App from './App';
@@ -35,7 +35,7 @@ const ListItem: FC<IListItemProps> = props => {
 }
 
 interface IListProps {
-    value: Content[];
+    value: Immutable.List<Content>;
     onRemove: (key: number) => void;
 }
 
@@ -56,24 +56,20 @@ const List: FC<IListProps> = props => {
 }
 
 const App: FC = () => {
-    const [value, setValue] = useState<Content[]>([]);
+    const [value, setValue] = useState<Immutable.List<Content>>(Immutable.List<Content>());
     const [index, setIndex] = useState<number>(0);
     const [add, setAdd] = useState<string>("");
 
     function onRemove(key: number): void {
-        let arr = value.map(i => i.clone());
-        const idx = arr.findIndex(i => i.key === key);
+        const idx = value.findIndex(i => i.key === key);
         if (idx !== -1) {
-            arr.splice(idx, 1);
-            setValue(arr);
+            setValue(value.splice(idx, 1));
         }
     }
 
     function onAdd(): void {
         if (add) {
-            let arr = value.map(i => i.clone());
-            arr.push(new Content(index + 1, add));
-            setValue(arr);
+            setValue(value.push(new Content(index + 1, add)));
             setIndex(index + 1);
             setAdd("");
         }
