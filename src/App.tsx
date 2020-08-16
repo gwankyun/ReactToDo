adb2b3ec78cb8bb0c97ca46e466da7b147ca4526
權少 <ljqican@163.com>
2020-08-16 22:46:28 +0800

    使用antd重構

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,96 @@
-import React from 'react';
+import React, { FC, useState, useCallback } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button, Switch, Radio, Input, List, Row, Col } from 'antd';
+import { useImmer } from 'use-immer';
 
-function App() {
+interface ToDo {
+  id: number;
+  content: string;
+  selected: boolean;
+  deleted: boolean;
+}
+
+const App: FC = () => {
+  const [current, setCurrent] = useState<string>('');
+  const [index, setIndex] = useState<number>(0);
+  const [list, upDateList] = useImmer<ToDo[]>([]);
+  const [showDeleted, setShowDeleted] = useState<boolean>(true);
+
+  const onAdd = useCallback((id: number, content: string) => {
+    upDateList(draft => {
+      draft.push({ id: id, content: content, selected: false, deleted: false });
+      setCurrent('');
+      setIndex(id + 1);
+    });
+  }, [upDateList]);
+
+  const onRemove = useCallback((todo: ToDo) => {
+    upDateList(draft => {
+      const idx = draft.findIndex(v => v.id === todo.id);
+      if (idx !== -1) {
+        draft[idx].deleted = !draft[idx].deleted;
+      }
+    })
+  }, [upDateList]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Row>
+        <Col span={20}>
+          <Input className="Input" value={current} onChange={e => setCurrent(e.target.value)} />
+        </Col>
+        <Col span={4}>
+          <Button className="Button"
+            onClick={_ => onAdd(index, current)}>添加</Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={2}>
+          <div style={{ float: "left", marginLeft: "10px" }}>
+            {"顯示完成項"}
+          </div>
+        </Col>
+        <Col span={22}>
+          <Switch className="Switch"
+            defaultChecked={showDeleted} onChange={c => setShowDeleted(c)} />
+        </Col>
+      </Row>
+      <List
+        dataSource={list.filter(v => showDeleted ? true : !v.deleted)}
+        renderItem={i =>
+          <List.Item
+            onMouseOver={_ => upDateList(draft => {
+              const idx = draft.findIndex(v => v.id === i.id);
+              if (idx !== -1) {
+                draft[idx].selected = true;
+              };
+            })}
+            onMouseLeave={_ => upDateList(draft => {
+              const idx = draft.findIndex(v => v.id === i.id);
+              if (idx !== -1) {
+                draft[idx].selected = false;
+              };
+            })}
+          >
+            <div className={i.deleted ? "ToDo-deleted" : "ToDo"}>
+              <Row>
+                <Col span={20}>
+                  <div className={i.deleted ? "Content-deleted" : "Content"}>
+                    {i.content}
+                  </div>
+                </Col>
+                <Col span={4}>
+                  <Button className="Button"
+                    style={{ display: i.selected ? 'block' : 'none' }}
+                    onClick={_ => onRemove(i)}>{i.deleted ? "取消" : "完成"}</Button>
+                </Col>
+              </Row>
+            </div>
+          </List.Item>
+        } />
     </div>
   );
-}
+};
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,42 +1,15 @@
-import React, { FC, useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import { List } from './list';
-import { Button } from './button';
-import { TextInput } from './text-input';
-import { useServer, Item } from './use-server';
-
-const App: FC = () => {
-  const server = useServer();
-
-  return (
-    <div>
-      <div className='Item'>
-        <TextInput value={server.value}
-          onChange={v => server.setValue(v)} />
-        <Button onClick={() => server.onAdd()}>添加</Button>
-      </div>
-      <List value={server.array} onItemRender={(v, i) => {
-        let item = v as Item;
-        return (
-          <li key={item.id} className='Item'>
-            <div>
-              <TextInput value={item.value}
-                onChange={v => server.onUpdate(i, v)} />
-              <Button onClick={() => server.onRemove(i)}>刪除</Button>
-              <Button onClick={() => server.onTop(i)}>置頂</Button>
-            </div>
-          </li>
-        );
-      }} />
-    </div>
-  );
-};
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
